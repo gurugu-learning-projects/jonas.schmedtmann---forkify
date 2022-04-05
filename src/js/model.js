400a57520e5ee62a5ed96bedc2527b1e268c6ffd
GuRuGu <peter.k.nets@gmail.com>
2022-04-05 18:40:27 +0300

    Show results for one page

--- src/js/model.js
+++ src/js/model.js
@@ -1,4 +1,4 @@
-import { API_URL } from './config.js';
+import { API_URL, RESULTS_PER_PAGE } from './config.js';
 import { getJSON } from './helpers.js';
 
 export const state = {
@@ -6,6 +6,8 @@ export const state = {
   search: {
     query: '',
     results: [],
+    page: 1,
+    resultsPerPage: 10,
   },
 };
 
@@ -48,4 +50,13 @@ export const loadSearchResults = async function (query) {
   } catch (error) {
     throw error;
   }
+};
+
+export const getSearchResultsPage = function (page = state.search.page) {
+  state.search.page = page;
+
+  const start = (page - 1) * RESULTS_PER_PAGE; // 0
+  const end = page * RESULTS_PER_PAGE; // 9
+
+  return state.search.results.slice(start, end);
 };
