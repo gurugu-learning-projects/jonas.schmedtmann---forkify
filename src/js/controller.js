9176d268ea4189b0a7a437d8946e0bb66072fdc8
GuRuGu <peter.k.nets@gmail.com>
2022-04-12 19:19:06 +0300

    Show bookmark list

--- src/js/controller.js
+++ src/js/controller.js
@@ -6,6 +6,7 @@ import recipeView from './views/recipeView.js';
 import searchView from './views/searchView.js';
 import resultsView from './views/resultsView.js';
 import paginationView from './views/paginationView.js';
+import bookmarksView from './views/bookmarksView.js';
 
 // https://forkify-api.herokuapp.com/v2
 
@@ -20,8 +21,9 @@ const controlRecipes = async function () {
     if (!id) return;
     recipeView.renderSpinner();
 
-    // 0. Update results view to mark selected result
+    // 0. Update results view and bookmarks view to mark selected result
     resultsView.update(model.getSearchResultsPage());
+    bookmarksView.update(model.state.bookmarks);
 
     // 1. Load recipe
     await model.loadRecipe(id);
@@ -70,13 +72,18 @@ const controlServings = function (newServings) {
 };
 
 const controlAddBookmark = function () {
+  // 1) Add/remove bookmark
   if (model.state.recipe.bookmarked) {
     model.removeBookmark(model.state.recipe.id);
   } else {
     model.addBookmark(model.state.recipe);
   }
 
+  // 2) Update recipe view
   recipeView.update(model.state.recipe);
+
+  // 3) Render bookmarks
+  bookmarksView.render(model.state.bookmarks);
 };
 
 const init = function () {
